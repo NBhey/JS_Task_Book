8a17b919c9aaec769096a3fb857662c57a4b63dc
Oganes <og.marckaryan@yandex.ru>
2025-04-13 21:15:54 +0300

    13.04.2025: поставил оставшиеся пакеты, начал писать webpack config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+};
