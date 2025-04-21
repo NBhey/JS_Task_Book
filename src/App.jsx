91706c15dfee80d424133475e3cb5d0d58d0e198
Oganes <og.marckaryan@yandex.ru>
2025-04-21 20:20:32 +0300

    21.04.2025: приступид к созданию компонентов

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function App(){
+    return (<h1> Все только начинается!</h1>)
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-// test.js
-const sum = (a, b) => a + b;
-console.log(sum(2, 3));
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import App from './App.jsx'
+import store from './store/store.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
