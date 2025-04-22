3861297d099ae24d60110dfd1187a9b1e22f28f3
Oganes <og.marckaryan@yandex.ru>
2025-04-22 21:03:44 +0300

    22.04.2025: настройка react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import "./App.css";
 
-export default function App(){
-    return (<h1> Все только начинается!</h1>)
+import HomePage from "./pages/HomePage.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <NavigationMenu />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function NavigationMenu() {
+  return (
+    <nav>
+      <NavLink to="/">Главная</NavLink>
+      <NavLink to="/tasks">Задачи</NavLink>
+    </nav>
+  );
 }
