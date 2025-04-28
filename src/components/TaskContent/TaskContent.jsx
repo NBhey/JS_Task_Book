ac42cf33ccf0521e8649d80866680eca33bf7f59
Oganes <og.marckaryan@yandex.ru>
2025-04-28 21:53:59 +0300

    28.04.2025: разбираюсь с prismjs

--- src/components/TaskContent/TaskContent.jsx
+++ src/components/TaskContent/TaskContent.jsx
@@ -1,33 +1,50 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-javascript";
+import "./TaskContent.css";
 
 const TaskContent = () => {
-    const id = useParams().taskId
+    const { taskId } = useParams();
+    const tasks = useSelector((state) => state.request.tasksArray);
+    
+    const currentTask = tasks.find(task => task.theme === taskId);
+    
+    React.useEffect(() => {
+        Prism.highlightAll();
+    }, [currentTask]);
 
-    const tasks = useSelector((state) => state.request.tasksArray); 
-    let codeArray
-
-    for (let task of tasks){
-        if(task.theme === id) {
-            codeArray = task.code.map((el) => el)
-        }
+    if (!currentTask) {
+        return <div className="task-content">Задача не найдена</div>;
     }
-    console.log(codeArray)
-//   const { taskId } = useParams(); // Получаем параметр из URL
-//   console.log(taskId)
-  return (
-    <div className="task-content">
-      <h2>Задача: {id}</h2>
-      <ul>
-        {codeArray.map((el) => {
-            return <li>
-                {el}
-            </li>
-        })}
-      </ul>
-    </div>
-  );
+
+    return (
+        <div className="task-content">
+            <h2>Задача: {currentTask.themeRU}</h2>
+            <div className="code-list">
+                {currentTask.code.map((codeSnippet, index) => (
+                    <div key={index} className="code-item">
+                        <pre>
+                            <code className="language-javascript">
+                                {formatCode(codeSnippet)}
+                            </code>
+                        </pre>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+// Функция для форматирования кода
+const formatCode = (code) => {
+    return code
+        .replace(/;/g, ';\n')  // Перенос после точек с запятой
+        .replace(/{/g, '{\n')  // Перенос после открывающих скобок
+        .replace(/}/g, '\n}')  // Перенос перед закрывающими скобок
+        .replace(/\) => /g, ') =>\n'); // Перенос после стрелок
 };
 
 export default TaskContent;
